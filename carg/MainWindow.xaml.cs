debf14178d44a1b63007dda88a8a5675ad111aec
Thomas <thomas.masiero26@gmail.com>
2024-04-02 10:05:30 -0300

    Adicionar arquivos de projeto.

---
+++ carg/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace carg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+
+                Result.Text = filename;
+
+                try
+                {
+                    using (var sr = new StreamReader(filename))
+                    {
+                        resultado.Text = await sr.ReadToEndAsync();
+                    }
+                }
+                catch (FileNotFoundException ex)
+                {
+                    resultado.Text = ex.Message;
+                }
+
+            }
+            
+        }
+    }
+}
